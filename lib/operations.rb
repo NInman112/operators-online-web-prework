77db52a54504dfbb0ba31040fd653cf83e6fb8bc
Nick Inman <nick@theoatmeal.com>
2018-11-16 00:31:46 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,22 @@
 def unsafe?(speed)
-
+if speed > 60
+  TRUE
+elsif speed < 40
+  TRUE
+else speed <= 60 && speed >= 40
+  FALSE
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	result = (speed > 60 ? true : false);
+	return result
+	result = (speed < 40 ? true : true);
+	return result
+	result = (speed >= 40 && speed <= 60 ? true : true);
+	return result
 end
 	
 
